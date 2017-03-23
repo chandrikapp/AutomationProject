37cb2b0f2c668612d30fb8234faaa0982e487c7b
home <home@192.168.0.6>
2017-03-23 21:10:40 +0530

    @print numbers

---
+++ JavaConceptsProject/src/pack12/PrintNumericValues.java
@@ -0,0 +1,16 @@
+package pack12;
+
+public class PrintNumericValues {
+	
+	
+	public static void main(String[] args) throws InterruptedException {
+		System.out.println("Print Numeric Values  :");
+		for(int i=1;i<=10;i++){
+			
+			System.out.print(i+"\t");
+			Thread.sleep(3000);
+			
+		}
+	}
+
+}
