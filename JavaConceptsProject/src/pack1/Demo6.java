b12e130dea18b8d007dfbe00996c0305c569c2bd
admin <admin@192.168.0.11>
2017-04-22 13:41:58 +0530

    @shiva_pack1_demo6

---
+++ JavaConceptsProject/src/pack1/Demo6.java
@@ -0,0 +1,25 @@
+package pack1;
+
+public class Demo6 {
+	int a=30;
+	static String name ="siva";
+	public void m1(){
+		int b=45;
+		System.out.print("hi shiva:"+" age:"+a+" name:"+name);
+		
+		
+
+}
+	public static void m2(){
+		int c=55;
+		Demo6 m=new Demo6();
+		System.out.println("hello "+"Age:"+m.a+c+name);
+		
+	}
+	public static void main(String[] args) {
+		Demo6 m=new Demo6();
+		m.m1();
+		m2();
+		
+	}
+}
